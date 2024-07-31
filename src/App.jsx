e88cd069789534329e6f27d349b77cbbd6388243
Pallavi-0880 <pallavithakur0880@gmail.com>
2024-07-31 15:20:51 +0530

    create main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from "./Components/Nav"
-import MainContent from './Components/MainContent'
+import Card from "./Components/Card"
 import Footer from "./Components/Footer"
+import Slideshow from './Components/Slideshow'
 
 const App =()=> {
  
+  const slides = [
+    {
+      image: 'https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=600',
+      heading: 'Heading 1',
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elitMaxime, iste nostrum atque laudantium voluptatem, eligendi quia voluptatibus corporis excepturi et laborum numquam'
+    },
+    {
+      image: 'https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=600',
+      heading: 'Heading 2',
+      text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, iste nostrum atque laudantium voluptatem, eligendi  quia voluptatibus corporis excepturi et laborum numquam'
+    },
+    {
+      image: 'https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=600',
+      heading: 'Heading 3',
+      text: 'This is the third slide'
+    }
+  ];
   return (
     <>
       <Nav/>
-      <MainContent/>
+      <Slideshow slides ={slides}/>
       <Footer/>
     </>
   )
